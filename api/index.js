01a06ed1a2e33e0b5331c1892b4356c11a5fe922
Moiz <moiz@webevis.com>
2024-03-15 21:47:40 +0500

    changes

--- api/index.js
+++ api/index.js
@@ -1,29 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => console.log('MongoDB is Connected!'))
-.catch(err => console.log(err));
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("MongoDB is Connected!"))
+  .catch((err) => console.log(err));
 
 const __dirname = path.resolve();
 
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");   
+  console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
@@ -31,26 +30,22 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.use(express.static(path.join(__dirname, "../client/build")));
+// Define your API routes or other server-side logic here// For any other requests, send the React app's HTML file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-
-
-app.get('/', (req, res) => {
-    res.send("Hello from Node API server!");
+app.get("/", (req, res) => {
+  res.send("Hello from Node API server!");
 });
-
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });    
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
-
